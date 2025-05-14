288e0d8a836db3ca6268f2918f5ec830ccaac7a8
Daniel <127387437+Daniel-Blvk@users.noreply.github.com>
2025-05-14 07:12:29 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -11,3 +11,27 @@ CROSS APPLY
 
 -- question 2
 
+
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+
+
+CREATE TABLE OrderItems (
+    OrderItemID INT IDENTITY(1,1) PRIMARY KEY,
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+
+INSERT INTO Orders (OrderID, CustomerName)
+SELECT DISTINCT OrderID, CustomerName
+FROM OrderDetails;
+
+
+INSERT INTO OrderItems (OrderID, Product, Quantity)
+SELECT OrderID, Product, Quantity
+FROM OrderDetails;
